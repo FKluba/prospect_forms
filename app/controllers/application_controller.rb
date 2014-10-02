20ceddb1054fa56950bc1736ec85e253546e08b4
François KLUBA <francois@appliserv.fr>
2014-10-02 15:29:46 +0200

    devise terminé, installation de cocoon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+	before_filter :authenticate_admin!, only: [:new, :edit, :create, :update, :destroy]
 
   def home
   	@title = "Formulaires destinés aux prospects"
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -5,6 +5,7 @@ class Admin
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
+key: username, String
+key: password, String
 
 end
